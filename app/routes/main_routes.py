69b75d57e8711a3b791616e03474b9fe93bc1c5b
Uriel Ondo <ondondoutoumouuriel@gmail.com>
2025-02-04 12:34:35 +0000

    ajout de header, du footer et contact

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -8,3 +8,7 @@ def index():
     # Récupérer tous les tableaux de la base de données
     tableaux = Tableau.query.all()
     return render_template('index.html', tableaux=tableaux)
+
+@main_bp.route('/contact')
+def contact():
+    return render_template('contact.html')
